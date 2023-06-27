70eb3be9fe3bd0c683eb7ccf2eb8f94cbf4d6b43
Laura <lauravieirapires@gmail.com>
2023-06-27 15:45:10 -0300

    finalizando arquivo index.html

--- Front-end/Secao1/Dia2-JS-assincrono-promises-e-fetch/index.js
+++ Front-end/Secao1/Dia2-JS-assincrono-promises-e-fetch/index.js
@@ -2,15 +2,15 @@
 const getDogButtom = document.querySelector("#random-dog");
 
 //pegando o botão random-cat
-
 const getCatButtom = document.querySelector("#random-cat");
 
-//pegando o elemento surprise me
-
-const surpriseButton = document.querySelector("#surprise-me")
 
 //pegando o elemento de imagem que irá exibir a imagem dos pets
 const petImage = document.querySelector("#random-pet-image");
+
+//pegando o elemento surprise me
+const surpriseButton = document.querySelector("#surprise-me")
+
 
 getDogButtom.addEventListener("click", () => {
 fetch("https://dog.ceo/api/breeds/image/random")
@@ -30,4 +30,17 @@ fetch("https://api.thecatapi.com/v1/images/search")
 
   petImage.src = petURL;
 });
+});
+
+surpriseButton.addEventListener("click", () => {
+  Promise.any([
+fetch("https://api.thecatapi.com/v1/images/search"),
+fetch("https://dog.ceo/api/breeds/image/random"),
+  ])
+  .then((response) => response.json())
+  .then((data) => {
+    const petURL = data.message || data[0].url;
+
+    petImage.src = petURL;
+  });
 });
