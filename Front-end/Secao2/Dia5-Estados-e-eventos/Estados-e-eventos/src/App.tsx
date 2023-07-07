3f818f329553e13f1775e054c80bc03093978045
Laura <lauravieirapires@gmail.com>
2023-07-07 16:18:29 -0300

    aplicação finalizada

--- Front-end/Secao2/Dia5-Estados-e-eventos/Estados-e-eventos/src/App.tsx
+++ Front-end/Secao2/Dia5-Estados-e-eventos/Estados-e-eventos/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-
 // src/App.tsx
 
 function App() {
@@ -15,9 +14,10 @@ function App() {
 
   // criando estado que contém o index da lista com valor inicial 0
 const [index, setIndex] = useState(0);
+const [toolList, setToolList] = useState(toolKit);
 
 function handleNextClick() {
-  if (index + 1 < toolKit.length) {
+  if (index + 1 < toolList.length) {
     setIndex(index + 1);
   } else {
     setIndex(0); // voltar para o primeiro elemento
@@ -27,18 +27,36 @@ function handlePreviousClick() {
   if (index -1 >= 0) {
     setIndex(index - 1);
   } else {
-    setIndex(toolKit.length - 1); // essa parte retorna para o último elemento da lista caso não tenha um item anterior
+    setIndex(toolList.length - 1); // essa parte retorna para o último elemento da lista caso não tenha um item anterior
   }
 }
+
+function handleAddClick() {
+  if (inputValue) {
+    setToolList([...toolList, inputValue]);
+  }
+}
+
+const [inputValue, setInputValue] = useState('');
 
   return (
     <>
       <h1>Caixa de ferramentas de uma Pessoa Desenvolvedora</h1>
-      <h2>{ toolKit[index] }</h2> 
+      <h2>{ toolList[index] }</h2> 
       <button onClick={handlePreviousClick}>Anterior</button>
       <button onClick={handleNextClick}>Próximo</button>
       <section>
         <h3>Adicione novas ferramentas</h3>
+        <input onChange={({ target }) => setInputValue(target.value)}/>
+        <button onClick={ handleAddClick }>Adicionar</button>   
+      </section>
+      <section>
+        <h3>Lista de itens do carrossel:</h3>
+        <ul>
+          {toolList.map((tool) => (
+            <li>{tool}</li>
+          ))}
+        </ul>
       </section>
     </>
   );
