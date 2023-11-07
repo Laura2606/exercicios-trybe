145497df1d93e237571dffb9e6766b5539205f28
Laura Vieira Pires <lauravieirapires@gmail.com>
2023-11-07 15:45:29 -0300

    Escrevendo dados em arquivos

--- Back-end/introducao-ao-desenvolvimentoweb-com-nodejs/dia-1-nodejs-runtime-assincrono/my-node-project/readFileSync.js
+++ Back-end/introducao-ao-desenvolvimentoweb-com-nodejs/dia-1-nodejs-runtime-assincrono/my-node-project/readFileSync.js
@@ -1,11 +1,27 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 const nomeDoArquivo = 'meu-arquivo.txt';
 
-try {
-  const data = fs.readFileSync(nomeDoArquivo, 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error(`Erro ao ler o arquivo: ${err.path}`);
-  console.log(err);
+// try {
+//   const data = fs.readFileSync(nomeDoArquivo, 'utf8');
+//   console.log(data);
+// } catch (err) {
+//   console.error(`Erro ao ler o arquivo: ${err.path}`);
+//   console.log(err);
+// }
+
+const fs = require('fs').promises;
+async function main() {
+    try {
+          const data = await fs.readFile(nomeDoArquivo, 'utf8');
+          console.log(data);
+        } catch (err) {
+          console.error(`Erro ao ler o arquivo: ${err.path}`);
+          console.log(err);
+        }
 }
+
+main()
+
+//Note que para podermos utilizar o async/await, precisamos criar uma função main e colocar nossa lógica dentro dela. 
+//Isso acontece porque o await só pode ser utilizado dentro de funções async.
