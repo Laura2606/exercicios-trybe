af896956061505868974a42a96050e255d7a0970
Laura Vieira Pires <lauravieirapires@gmail.com>
2023-11-07 20:13:33 -0300

    criando minha primeira API e utilizando vscode thunder client para fazer requisições

--- Back-end/introducao-ao-desenvolvimentoweb-com-nodejs/dia-2-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
+++ Back-end/introducao-ao-desenvolvimentoweb-com-nodejs/dia-2-nodejs-api-rest-com-express/soccer-team-manager/src/app.js
@@ -1,7 +1,34 @@
 // importando o pacote do express
 const express = require('express');
 
+const teams = [
+    {
+      id: 1,
+      name: 'São Paulo Futebol Clube',
+      initials: 'SPF',
+    },
+    {
+      id: 2,
+      name: 'Clube Atlético Mineiro',
+      initials: 'CAM',
+    },
+  ];
+
 // iniciando o express
 const app = express();
+
+app.use(express.json());
+
+// app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+
+// usando o endpoint do tipo GET
+app.get('/teams', (req, res) => res.status(200).json({ teams }));
+
+app.post('/teams', (req, res) => {
+    const newTeam = { ...req.body };
+    teams.push(newTeam);
+  
+    res.status(201).json({ team: newTeam });
+  });
 
 module.exports = app;
